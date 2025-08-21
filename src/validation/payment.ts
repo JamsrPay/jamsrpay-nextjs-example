d0e1f2b1f3baa4bd8a79485b8a532350895e97d5
Ankush kuamr <kumars701113@gmail.com>
2025-08-21 12:50:57 +0530

    fix: error

--- src/validation/payment.ts
+++ src/validation/payment.ts
@@ -1,5 +1,7 @@
 import { coerce, object } from "zod";
 
 export const paymentSchema = object({
-  amount: coerce.number().gt(10, "Amount must be greater than 10"),
+  amount: coerce
+    .number()
+    .gte(10, "Amount must be greater than 10 or equal to 10"),
 });
